0b6947a0e3b824e8d7c8346654a1523cf3a32091
AkashArul26 <95226047+AkashArul26@users.noreply.github.com>
2023-12-07 15:45:06 +0530

    Committing the PDF sample

--- WordToPDFWebApp/Controllers/HomeController.cs
+++ WordToPDFWebApp/Controllers/HomeController.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Syncfusion.DocIO.DLS;
-using Syncfusion.DocIO;
-using Syncfusion.DocIORenderer;
 using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Drawing;
+using System.IO;
 using System.Diagnostics;
 using WordToPDFWebApp.Models;
 
@@ -34,60 +34,27 @@ namespace WordToPDFWebApp.Controllers
         }
         public IActionResult WordToPDF(string button)
         {
-            string errorMessage = string.Empty;
-            if (button == null)
-                return View("Index");
- 
-            if (Request.Form.Files != null)
-            {
-                if (Request.Form.Files.Count == 0)
-                {
-                    ViewBag.Message = string.Format("Browse a Word document and then click the button to convert as a PDF document");
-                    return View("Index");
-                }
-                // Gets the extension from file.
-                string extension = Path.GetExtension(Request.Form.Files[0].FileName).ToLower();
-                // Compares extension with supported extensions.
-                if (extension == ".docx")
-                {
-                    MemoryStream stream = new MemoryStream();
-                    Request.Form.Files[0].CopyTo(stream);
-                    try
-                    {
-                        //Open using Syncfusion
-                        using (WordDocument document = new WordDocument(stream, FormatType.Docx))
-                        {
-                            stream.Dispose();
-                            // Creates a new instance of DocIORenderer class.
-                            using (DocIORenderer render = new DocIORenderer())
-                            {
-                                // Converts Word document into PDF document
-                                using (PdfDocument pdf = render.ConvertToPDF(document))
-                                {                                                                     
-                                    MemoryStream memoryStream = new MemoryStream();
-                                    // Save the PDF document
-                                    pdf.Save(memoryStream);
-                                    memoryStream.Position = 0;
-                                    return File(memoryStream, "application/pdf", "WordToPDF.pdf");
-                                }                                                           
-                            } 
-                        }                                                
-                    }
-                    catch (Exception ex)
-                    {
-                        ViewBag.Message = "Message : " + ex.Message.ToString() + "StackTrace : " + ex.StackTrace.ToString();
-                    }
-                }
-                else
-                {
-                    ViewBag.Message = string.Format("Please choose Word format document to convert to PDF");
-                }
-            }
-            else
-            {
-                ViewBag.Message = string.Format("Browse a Word document and then click the button to convert as a PDF document");
-            }
-            return View("Index");
+            //Create a new PDF document.
+            PdfDocument document = new PdfDocument();
+            //Add a page to the document.
+            PdfPage page = document.Pages.Add();
+
+            //Create PDF graphics for the page.
+            PdfGraphics graphics = page.Graphics;
+            FileStream fontStream = new FileStream(Path.GetFullPath(@"Data/arial.ttf"), FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+            //Use the font installed in the machine
+            PdfFont font = new PdfTrueTypeFont(fontStream, 14);
+            //Draw the text.
+            graphics.DrawString("Hello World!!!", font, PdfBrushes.Black, new PointF(0, 0));
+            //Saving the PDF to the MemoryStream.
+            MemoryStream stream = new MemoryStream();
+            document.Save(stream);
+            //Set the position as '0'.
+            stream.Position = 0;
+            //Download the PDF document in the browser.
+            FileStreamResult fileStreamResult = new FileStreamResult(stream, "application/pdf");
+            fileStreamResult.FileDownloadName = "Sample.pdf";
+            return fileStreamResult;
         }  
     }
 }
